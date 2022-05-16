ad57172f90cd03fd69deadfac0a91ca529e71b79
Yioshka <105658313+Yioshka@users.noreply.github.com>
2022-05-16 18:13:52 +0300

    Add files via upload

---
+++ testovoe/script/script.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+        $("#send").on("click","a", function (event) {
+            event.preventDefault();
+            var id  = $(this).attr('href'),
+                top = $(id).offset().top;
+            $('body,html').animate({scrollTop: top}, 2000);
+        });
+});
+       
